01aeb1bc98a6f0b230b1f7a792c070623a1b0d09
Daniel Legan <legan.daniel@gmail.com>
2022-09-07 16:46:56 -0500

    completed required objectives

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,46 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+//creating a new empty array
+let basket = [];
+
+//adding items to the basket array
+function addItem(item) {
+    basket.push(item); //adds item to end of basket
+    return true;
+}
+
+//printing each item of an array of unknown length
+function listItems(array) {
+    let i = 0;
+    while(i < array.length) {
+        console.log(array[i]);
+        i++;
+    }
+}
+
+//removing each item of an array and displaying eatch iteration
+function empty(array) {
+    while(array.length > 0){
+        array.pop(); //removes last item in an array
+        console.log(array);
+    }
+}
+
+/*--------------------*/
+console.log(`Basket is ${basket}`); //not sure what $ is used for
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log('Adding bananas (expect true)', addItem('bananas'));
+console.log('Adding oranges (expect true)', addItem('oranges'));
+console.log('Adding grapes (expect true)', addItem('grapes'));
+console.log(`Basket is now ${basket}`); //not sure what $ is used for
+
+//performing console.log of each element of the basket array
+listItems(basket);
+console.log(basket);
+
+//performing empty function
+empty(basket);
+//displaying contents of basket
+console.log(basket);
