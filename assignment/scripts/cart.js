27f640e6ff0f4341130cec0ce907279c149b1980
Daniel Legan <legan.daniel@gmail.com>
2022-09-08 14:03:57 -0500

    added isFull() function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,10 +4,11 @@ console.log('***** Cart Functions *****');
 
 //creating a new empty array
 let basket = [];
+const maxItems = 5;
 
-//adding items to the basket array
+//adding an item to the basket
 function addItem(item) {
-    basket.push(item); //adds item to end of basket
+    basket.push(item);
     return true;
 }
 
@@ -22,18 +23,25 @@ function listItems(array) {
 
 //removing each item of an array and displaying eatch iteration
 function empty(array) {
-    while(array.length > 0){
+    while(array.length > 0) {
         array.pop(); //removes last item in an array
     }
 }
 
+//checks if basket is full or not
+function isFull(array) {
+    if(array.length < maxItems) {
+        return false;
+    }else {
+        return true;
+    }
+}
+
 /*--------------------*/
-console.log(`Basket is ${basket}`); //not sure what $ is used for
 console.log('Adding apples (expect true)', addItem('apples'));
 console.log('Adding bananas (expect true)', addItem('bananas'));
 console.log('Adding oranges (expect true)', addItem('oranges'));
 console.log('Adding grapes (expect true)', addItem('grapes'));
-console.log(`Basket is now ${basket}`); //not sure what $ is used for
 
 //performing console.log of each element of the basket array
 listItems(basket);
@@ -42,5 +50,9 @@ console.log(basket);
 empty(basket);
 addItem('tomato');
 addItem('celery');
+addItem('celery');
+addItem('celery');
+
 //displaying contents of basket
 console.log(basket);
+console.log(isFull(basket));
